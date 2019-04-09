607ca6004bfce188fa4204c4bd50a937fdb32294
bftv <bftvtech@ucdavis.edu>
2019-04-09 16:49:23 -0700

    Update script.js

--- vue_blocks/vue_news_block/script.js
+++ vue_blocks/vue_news_block/script.js
@@ -1,4 +1,10 @@
-/* Global functions */
+/* Main URLs */
+newsSiteURL = "https://news.bftv.ucdavis.edu";
+APIurl = newsSiteURL+"/jsonapi/node/news_article?sort=-created";
+const blockID = document.getElementsByClassName('vue-news-block')[0].id;
+/* End Main URLs */
+
+/* Global Filters */
 
 Vue.filter('imgstriptruncate', function (text, stop, clamp) {
 	var content = text.slice(0, stop) + (stop < text.length ? clamp || '...' : '');
@@ -6,78 +12,137 @@ Vue.filter('imgstriptruncate', function (text, stop, clamp) {
 });
 Vue.filter('fiximg', function (text) {
 	var content = text;
-	return content.replace(new RegExp('src="/sites', 'g'), 'class="inline-img" src="'+mainURL+'/sites');
+	return content.replace(new RegExp('src="/sites', 'g'), 'class="inline-img" src="'+newsSiteURL+'/sites');
 });
 
-/* End Global functions */
+/* End Global Filters */
  
-var mainURL = "https://news.bftv.ucdavis.edu";
-const url = mainURL+"/jsonapi/node/news_article?sort=-created&filter[category][condition][path]=field_fbtv_news_for.uuid&filter[category][condition][operator]=%3D&filter[category][condition][value]=a136800e-308a-4672-a42d-141b4cb9e594&include=field_sf_primary_image&page[limit]=15";
 
-const vm = new Vue({
-	el: '#news-block',
-	data: {
-		newsData: null,
-		newsList: true,
-		newsItem: null,
-		imgsrc: '',
-		newsdocurl: '',
-		newsdocname: '',
-		thumbnails: '',
-		thumbnailURL: '',
-		includeindex: [],
-		moment: moment,
-		loading: true
+/* Components */
+
+var newsList = Vue.extend({
+    template: '#news-list-template',
+
+    data: function() {
+        return {
+            newsData: null,			
+			thumbnails: '',			
+			moment: moment,
+			loading: true,
+			itemsShown: 10,
+			listShow: false,
+			department: 'bftv'
+        }
+    },
+	
+	mounted: function() {
+		this.itemsShown = drupalSettings.pdb.configuration[blockID].itemsPerPage,
+		this.listShow = drupalSettings.pdb.configuration[blockID].ShowListing,
+		this.department = drupalSettings.pdb.configuration[blockID].Department,
+		firstURL = this.URLBuilder(this.department),
+		finalURL = APIurl+firstURL+'&page[limit]='+this.itemsShown,
+		this.getNewsList(finalURL)	
 	},
-	mounted() {
-		axios.get(url).then(response => {
-			//this.newsData = response.data.data[0].relationships.field_fbtv_news_for.data.id
-			this.newsData = response.data.data,			
-			this.thumbnails = response.data.included,
-			this.loading = false
-		})
-	},
-	methods: {
-		loadNews: function(newsid){
-			this.loading = true,
-			window.scrollTo(0,0),
-			newsURL = mainURL+"/jsonapi/node/news_article/"+newsid+"?include=field_sf_primary_image",
-			this.newsList = false,
-			axios.get(newsURL).then(response => {
-				this.newsItem = response.data.data,
-				this.imgsrc = response.data.included ? mainURL+response.data.included[0].attributes.url : '',				
-				this.loading = false				
-			})
-		},
-		GoBack: function() {
-			this.loading = true,
-			this.newsList = true,
-			this.imgsrc = '',
-			this.newsdocurl = '',
-			this.newsdocname = '',
-			this.loading = false
-		},
-		loadNewsDoc: function(newsid){
-			newsURL = mainURL+"/jsonapi/file/file/"+newsid,
-			axios.get(newsURL).then(response => {
-				this.newsdocurl = mainURL+response.data.data.attributes.url,
-				this.newsdocname = response.data.data.attributes.filename
+
+    methods: {
+        getNewsList: function(url){
+				axios.get(url).then(response => {			
+				this.newsData = response.data.data,			
+				this.thumbnails = response.data.included,
+				this.loading = false
 			})
 		},
 		findthumbnail: function(imgid) {
 			for(var i=0; i < this.thumbnails.length; i++){
 				if(this.thumbnails[i].id == imgid){
-					return mainURL+"/sites/g/files/dgvnsk1131/files/styles/sf_thumbnail/public"+this.thumbnails[i].attributes.uri.value.substr(8)
+					return newsSiteURL+"/sites/g/files/dgvnsk1131/files/styles/sf_thumbnail/public"+this.thumbnails[i].attributes.uri.value.substr(8)
 				}
 			}			
 		},
-		newsblurb: function(newsid) {
-			for(var i=0; i < this.newsData.length-1; i++){
-				if(this.newsData[i].id == newsid){
-					return this.newsData[i].attributes.body.value;
-					//console.log(this.newsData[i].attributes.body.value)
-				}
+		URLBuilder: function(path){	
+			if(path == 'bftv'){
+				return '&include=field_sf_primary_image'
+			}
+			else if(path == 'bae'){
+				return '&filter[category][condition][path]=field_fbtv_news_for.uuid&filter[category][condition][operator]=%3D&filter[category][condition][value]=9a04d3a5-d0b8-4c0b-8649-11fb9179759a&include=field_sf_primary_image'
+			}
+			else if(path == 'fst'){
+				return '&filter[category][condition][path]=field_fbtv_news_for.uuid&filter[category][condition][operator]=%3D&filter[category][condition][value]=ce16a904-890b-4ddd-8954-41b13dea0b3d&include=field_sf_primary_image'
+			}
+			else if (path == 'ven'){
+				return '&filter[category][condition][path]=field_fbtv_news_for.uuid&filter[category][condition][operator]=%3D&filter[category][condition][value]=a136800e-308a-4672-a42d-141b4cb9e594&include=field_sf_primary_image'
+			}
+			else if (path == 'txc'){
+				return '&filter[category][condition][path]=field_fbtv_news_for.uuid&filter[category][condition][operator]=%3D&filter[category][condition][value]=4eed687e-715b-405f-b5fb-1a32039ec0f3&include=field_sf_primary_image'
 			}
 		}
-	}
+	},
+})
+
+var singleNews = Vue.extend({
+    template: '#single-news-template',
+
+    data: function(){
+        return {		
+			newsItem: null,
+			imgsrc: '',
+			newsdocurl: '',
+			newsdocname: '',
+			moment: moment,
+			loading: true
+        }
+    },
+
+    mounted: function(){
+        this.getTheNews();
+    },
+
+    methods: {
+        getTheNews: function(){
+            newsURL = newsSiteURL+"/jsonapi/node/news_article/"+this.$route.params.newsID+"?include=field_sf_primary_image",
+			axios.get(newsURL).then(response => {
+				this.newsItem = response.data.data,
+				this.imgsrc = response.data.included ? newsSiteURL+response.data.included[0].attributes.url : '',				
+				this.loading = false				
+			})
+        },
+		loadNewsDoc: function(docID){
+			newsURL = newsSiteURL+"/jsonapi/file/file/"+docID,
+			axios.get(newsURL).then(response => {
+				this.newsdocurl = newsSiteURL+response.data.data.attributes.url,
+				this.newsdocname = response.data.data.attributes.filename
+			})
+		},
+		goBack: function(){
+			router.go(-1)
+		}
+    }
+})
+
+
+/* End Components */
+
+/* Router */
+
+var router = new VueRouter({
+	mode: 'history',
+	scrollBehavior() {
+		return { x: 0, y: 0 };
+	},
+	routes: [
+	//{ path: '/about/news-events/department-news/', component: newsList },
+	{ path: '*', component: newsList },
+	{ path: '/about/news-events/department-news/:newsID', name: 'news', component: singleNews }
+	]
 });
+
+/* End Router */
+
+/* Initialize */
+
+new Vue({
+	el: '#news-block',
+	router
+})
+
+/* End Initialize */
