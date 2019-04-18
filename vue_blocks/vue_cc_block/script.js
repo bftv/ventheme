bfdd2237e25f67d85eb76301d87a09a97e1b7a70
bftv <bftvtech@ucdavis.edu>
2019-04-18 16:41:02 -0700

    Create script.js

---
+++ vue_blocks/vue_cc_block/script.js
@@ -0,0 +1,156 @@
+/* Main URLs */
+ccDbUrl = "http://vendept.ucdavis.edu:8082/culturecollection/getrecord";
+const blockID = document.getElementsByClassName('vue-cc-block')[0].id;
+/* End Main URLs */
+
+
+/* Components */
+
+var recordList = Vue.extend({
+    template: '#record-list-template',
+
+    data: function() {
+        return {
+            recordsData: null,				
+			loading: true,
+			currentPage: 0,
+			pageSize: 5,
+			visibleRecords: [],
+			s_strain_no: null,
+			s_type: null,
+			s_genus: null,
+			s_species: null,
+			s_strain: null,
+			s_other_no: null,
+			s_source: null,
+			s_synonyms: null
+        }
+    },
+	
+	mounted: function() {
+		//this.pageSize = drupalSettings.pdb.configuration[blockID].recordsPerPage,
+		this.getRecordsList(ccDbUrl)
+	},
+
+    methods: {
+        getRecordsList: function(url){
+				axios.get(url).then(response => {			
+				this.recordsData = response.data,
+				this.updateVisibleRecords(),
+				this.loading = false				
+			})
+		},
+		updatePage: function(pageNumber){
+			this.currentPage = pageNumber;
+			this.updateVisibleRecords();
+		},
+		updateVisibleRecords: function(){
+			this.visibleRecords = this.recordsData.slice(this.currentPage * this.pageSize, (this.currentPage * this.pageSize) + this.pageSize);
+			if(this.visibleRecords.length == 0 && this.currentPage > 0) {
+				this.updatePage(this.currentPage - 1);
+			}
+		},
+		totalPages: function() {
+			return Math.ceil(this.recordsData.length / this.pageSize);
+		},
+		showPreviousLink: function() {
+			return this.currentPage == 0 ? false : true;
+		},
+		showNextLink: function() {
+			return this.currentPage == (this.totalPages() - 1) ? false : true;
+		},
+		search: function(){
+			this.loading = true,
+			searchURL = ccDbUrl+'?';
+			if(this.s_strain_no){
+				searchURL += 'strain_no='+this.s_strain_no+'&'
+			}
+			if(this.s_type){
+				searchURL += 'type='+this.s_type+'&'
+			}
+			if(this.s_genus){
+				searchURL += '&genus='+this.s_genus+'&'
+			}
+			if(this.s_species){
+				searchURL += '&species='+this.s_species+'&'
+			}
+			if(this.s_strain){
+				searchURL += '&strain='+this.s_strain+'&'
+			}
+			if(this.s_other_no){
+				searchURL += '&other_no='+this.s_other_no+'&'
+			}
+			if(this.s_source){
+				searchURL += '&source='+this.s_source+'&'
+			}
+			if(this.s_synonyms){
+				searchURL += '&synonyms='+this.s_synonyms+'&'
+			}
+			this.getRecordsList(searchURL) 
+		}
+	},
+})
+
+var singleRecord = Vue.extend({
+    template: '#single-record-template',
+
+    data: function(){
+        return {		
+			record: null,			
+			loading: true
+        }
+    },
+
+    mounted: function(){
+        this.getThisRecord();
+    },
+
+    methods: {
+        getThisRecord: function(){
+            recordURL = 'http://vendept.ucdavis.edu:8082/culturecollection/getrecord?id='+this.$route.params.recordID,
+			axios.get(recordURL).then(response => {
+				this.record = response.data[0],
+				console.log(this.record),
+				this.loading = false				
+			})
+        },		
+		goBack: function(){
+			router.go(-1)
+		}
+    }
+})
+
+
+/* End Components */
+
+/* Router */
+
+var router = new VueRouter({
+	//mode: 'history',
+	scrollBehavior() {
+		return { x: 0, y: 0 };
+	},
+	
+	routes: [
+		{ 
+			path: '*', 
+			component: recordList 
+		},
+		{ 
+			path: '/record/:recordID', 
+			name: 'record', 
+			component: singleRecord
+		}
+	]
+});
+
+/* End Router */
+
+/* Initialize */
+
+new Vue({
+	el: '#cc-block',
+	router
+})
+
+/* End Initialize */
