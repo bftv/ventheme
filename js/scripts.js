742aa366fbf9fd7ec258b256e0ba40638267bcd6
bftv <bftvtech@ucdavis.edu>
2023-09-21 12:00:43 -0700

    Update scripts.js
    Change the title for VENJobs Submissions.

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,16 @@
   };
   
 })(jQuery, Drupal);
+jQuery(document).ready(function ($) {
+  $(function(){
+		if (window.location.pathname.indexOf('/jobs-post/submissions') !== -1) {
+			// Change the webform title in the <h1> element.
+			var customTitle = 'Your Posted Jobs';
+			var pageTitleElement = document.querySelector('.page-title');
+
+			if (pageTitleElement) {
+			  pageTitleElement.textContent = customTitle;
+			}
+		}
+	});  
+});
